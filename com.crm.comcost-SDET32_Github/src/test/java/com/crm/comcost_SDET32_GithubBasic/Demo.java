a7103facbe32120c3c686dc9f7ca3496dd4a9b42
Brijesh <Brijesh@LAPTOP-JU92Q6OF>
2022-04-12 11:37:01 +0530

    Commit Ist

---
+++ com.crm.comcost-SDET32_Github/src/test/java/com/crm/comcost_SDET32_GithubBasic/Demo.java
@@ -0,0 +1,13 @@
+package com.crm.comcost_SDET32_GithubBasic;
+
+import org.testng.annotations.Test;
+
+public class Demo
+{
+	@Test
+	public void demoTest()
+	{
+		System.out.println("Ist github program");
+	}
+
+}
