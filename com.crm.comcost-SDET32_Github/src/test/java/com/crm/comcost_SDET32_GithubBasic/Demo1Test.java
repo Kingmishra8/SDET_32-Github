c8066afa6ef988dd5bcaf9da2f436b9a00053394
Brijesh <Brijesh@LAPTOP-JU92Q6OF>
2022-04-14 12:30:47 +0530

    Parametrization

--- com.crm.comcost-SDET32_Github/src/test/java/com/crm/comcost_SDET32_GithubBasic/Demo1Test.java
+++ com.crm.comcost-SDET32_Github/src/test/java/com/crm/comcost_SDET32_GithubBasic/Demo1Test.java
@@ -1,5 +1,7 @@
 package com.crm.comcost_SDET32_GithubBasic;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class Demo1Test
@@ -7,7 +9,14 @@ public class Demo1Test
 	@Test
 	public void DemeoTest1()
 	{
+		WebDriver driver=new ChromeDriver();
+		String BROWSER= System.getProperty("browser");
+		String URL= System.getProperty("url");
+		System.out.println(BROWSER);
+		System.out.println(URL);
+		driver.get(URL);
 		System.out.println("Pushing 2nd class");
+		System.out.println("Parameterrization");
 	}
 
 }
